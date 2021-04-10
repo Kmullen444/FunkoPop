c4f81cdcda4e0c686d890c2ac771ddd4a4f48254
kmullen444 <kmullen444@gmail.com>
2021-04-10 12:02:05 -0700

    Saving work so far

---
+++ the_odin_project/tic_tac_toe/lib/tic_tac_toe.rb
@@ -0,0 +1,8 @@
+require "tic_tac_toe/version"
+
+module TicTacToe
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+require relative "./ tic tac toe/cell.rb"
---
+++ the_odin_project/tic_tac_toe/lib/tic_tac_toe/cell.rb
@@ -0,0 +1,8 @@
+module TicTacToe
+  class Cell
+    attr_accessor :value
+    def intialize(value = "")
+      @value = value
+    end
+  end
+end
